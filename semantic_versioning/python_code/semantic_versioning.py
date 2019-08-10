e180bab9bd71835f9b72d0d30d4ca720ecc6548b
Brad Anderson <brad.anderson58@gmail.com>
2019-08-10 04:19:16 -0400

    test different error messages

--- semantic_versioning/python_code/semantic_versioning.py
+++ semantic_versioning/python_code/semantic_versioning.py
@@ -10,8 +10,23 @@ class SemanticVersionQuestion:
 
     @staticmethod
     def get_greater_version(first_version: str, second_version: str) -> str:
+        if first_version is None or second_version is None:
+            raise SemanticVersionException('Invalid Format: Null value detected')
+
         first_version_list = first_version.split('.')
         second_version_list = second_version.split('.')
+
+        if len(first_version_list) != 3 or len(second_version_list) != 3:
+            raise SemanticVersionException('Invalid Format: Values must take the form X.Y.Z')
+
+        empty = next(filter(lambda s: s is '', first_version_list + second_version_list), None)
+        if empty is not None:
+            raise SemanticVersionException('Invalid Format: Values must take the form X.Y.Z')
+
+        non_numeric = next(filter(lambda s: invalid_value(s), first_version_list + second_version_list), None)
+        if non_numeric is not None:
+            raise SemanticVersionException('Invalid Format: Non-numeric values detected')
+
         for first, second in zip(first_version_list, second_version_list):
             if first > second:
                 return first_version
@@ -19,8 +34,13 @@ class SemanticVersionQuestion:
                 return second_version
             else:
                 continue
-        return '0.0.0'
+        return first_version
 
 
-class VersionMismatchException(Exception):
-    pass
+class SemanticVersionException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+def invalid_value(s: str) -> bool:
+    return not s.isdigit()
