95a2eef939adedbf39e17afa3b8fd1958317c149
Brad Anderson <brad.anderson58@gmail.com>
2019-08-10 05:11:32 -0400

    fix whitespace

--- remove_dups/javascript/remove_dup.spec.js
+++ remove_dups/javascript/remove_dup.spec.js
@@ -11,21 +11,21 @@ describe('Removing Duplicates', () => {
   });
 
   it('should remove duplicates out of order', () => {
-  	var noDuplicates = listUtil.removeDuplicates([5, 2, 4, 6, 2, 7, 4, 99, 5, 2, 11]);
+    var noDuplicates = listUtil.removeDuplicates([5, 2, 4, 6, 2, 7, 4, 99, 5, 2, 11]);
 
-  	assert.deepEqual(noDuplicates, [5, 2, 4, 6, 7, 99, 11]);
+    assert.deepEqual(noDuplicates, [5, 2, 4, 6, 7, 99, 11]);
   });
 
   it('should work for strings as well', () => {
-  	var noDuplicates = listUtil.removeDuplicates(['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish'])
+    var noDuplicates = listUtil.removeDuplicates(['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish'])
 
-  	assert.deepEqual(noDuplicates, ['one', 'fish', 'two', 'red', 'blue']);
+    assert.deepEqual(noDuplicates, ['one', 'fish', 'two', 'red', 'blue']);
   });
 
   it('should work for objects also', () => {
-  	var noDuplicates = listUtil.removeDuplicates([{one: 'fish'}, {two: 'fish'}, {one: 'fish'}, {one: 'blue'}]);
+    var noDuplicates = listUtil.removeDuplicates([{one: 'fish'}, {two: 'fish'}, {one: 'fish'}, {one: 'blue'}]);
 
-  	assert.deepEqual(noDuplicates, [{one: 'fish'}, {two: 'fish'}, {one: 'blue'}]);
+    assert.deepEqual(noDuplicates, [{one: 'fish'}, {two: 'fish'}, {one: 'blue'}]);
   });
 
 });
