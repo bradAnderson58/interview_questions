4b21a2473c42c0162caa27c32907a40b13943ea5
Brad Anderson <brad.anderson58@gmail.com>
2019-08-10 05:08:37 -0400

    reset test question

--- remove_dups/javascript/remove_dups.js
+++ remove_dups/javascript/remove_dups.js
@@ -12,19 +12,6 @@ function ListUtility() {
     Removes duplicate entries from a list
    */
   function removeDuplicates(dupsList) {
-    var firsts = {};
-    var noDups = []
-    for (ind in dupsList) {
-      var key = dupsList[ind];
-      if (typeof(key) === 'object') {
-        key = JSON.stringify(key);
-      } 
-
-      if (!firsts[key]) {
-        firsts[key] = true;
-        noDups.push(dupsList[ind])
-      }
-    }
-    return noDups;
+    return dupsList;
   }
 }
