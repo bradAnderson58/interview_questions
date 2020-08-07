6b252b77725bad26be1edbcac92430e5f45803f1
Andrew Hall <ryzngard@live.com>
2020-08-07 16:47:08 -0700

    Add palindrome

---
+++ csharp_code/palindrome/Palindrome.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace palindrome
+{
+    static class Palindrome
+    {
+        public static bool IsPalindrome(this string str)
+            => str is null
+            ? false
+            : str.Reverse().SequenceEqual(str);
+    }
+}
---
+++ csharp_code/palindrome/PalindromeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace palindrome
+{
+    public class PalindromeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
