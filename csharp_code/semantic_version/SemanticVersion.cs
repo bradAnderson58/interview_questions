7d0112824d2be1ce0a881617c5bcddbbef9a32c0
Andrew Hall <ryzngard@live.com>
2020-08-07 16:18:02 -0700

    Add semantic_version

---
+++ csharp_code/semantic_version/SemanticVersion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace semantic_version
+{
+    static class SemanticVersion
+    {
+        public static string GetGreaterVersion(string v1, string v2)
+        {
+            var splitV1 = v1?.Split(".").Where(s => s != "").ToArray();
+            var splitV2 = v2?.Split(".").Where(s => s != "").ToArray();
+
+            if (splitV1 is null ||
+                splitV2 is null ||
+                splitV1.Length + splitV1.Length != 6)
+            {
+                throw new InvalidFormatException();
+            }
+
+            
+            foreach (var (first, second) in splitV1.Zip(splitV2))
+            {
+                if (!int.TryParse(first, out var firstInt))
+                    throw new NonNumericValueException();
+
+                if (!int.TryParse(second, out var secondInt))
+                    throw new NonNumericValueException();
+
+                if (firstInt > secondInt)
+                    return v1;
+                else if (secondInt > firstInt)
+                    return v2;
+            }
+
+            return v1;
+        }
+
+    }
+
+    class NonNumericValueException : Exception
+    {
+
+    }
+
+    class InvalidFormatException : Exception
+    {
+
+    }
+}
---
+++ csharp_code/semantic_version/SemanticVersionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace semantic_version
+{
+    public class SemanticVersionTests
+    {
+        [Theory]
+        [InlineData("1.1.0", "1.2.0", "1.2.0")]
+        [InlineData("1.2.0", "1.2.0", "1.2.0")]
+        public void Tests(string arg1, string arg2, string answer)
+        {
+            Assert.Equal(answer, SemanticVersion.GetGreaterVersion(arg1, arg2));
+        }
+
+        [Theory]
+        [InlineData("1.a.0", "1.2.0", typeof(NonNumericValueException))]
+        [InlineData("", "1.2.0", typeof(InvalidFormatException))]
+        [InlineData(null, "1.2.0", typeof(InvalidFormatException))]
+        [InlineData("1.2.3.4.5.6", "1.2.0", typeof(InvalidFormatException))]
+        [InlineData("1..6", "1.2.0", typeof(InvalidFormatException))]
+        [InlineData("1.6", "1.2.0", typeof(InvalidFormatException))]
+        public void ExceptionTests(string arg1, string arg2, Type exceptionType)
+        {
+            Assert.Throws(exceptionType, () =>
+            {
+                SemanticVersion.GetGreaterVersion(arg1, arg2);
+            });
+        }
+    }
+}
